268de7d1dd3db7f9390f783c0c6139f585c524e7
Diana Sushik <dianasushik2000@gmail.com>
2022-08-09 13:46:12 -0700

    commit tree 2b17f90fde40e3c9eb9190d604190905f52b3522

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
-// Code your solutions in this file
+const sinon = 'sinon';
+let spy = 'spy';
+
+const names = ["Guadalupe", "Ollie", "Aki"]; 
+const eventName = "surprise";
+let message = [];
+
+function writeCards(names, eventName) {
+    for (let i = 0; i < names.length; i++){
+     const newMessage = `Thank you, ${names[i]}, for the wonderful ${eventName} gift!`;
+    message.push(newMessage);
+    }
+    return message;
+}
+
+
+function countDown(number) {
+    let i = 10;
+while (i >= 0)
+    console.log(i--);
+}
+
+
